08e6ba13cfa4f3c8ab4f5146d268eb103c09efda
Furtado <furttado@gmail.com>
2020-08-03 21:46:18 -0300

    TeacherList and TeacherForm components created

---
+++ web/src/pages/TeacherForm/TeacherForm.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function TeacherForm() {
+  return (
+    <div>
+      <h1>TeacherForm</h1>
+    </div>
+  );
+}
---
+++ web/src/pages/TeacherList/TeacherList.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function TeacherList() {
+  return (
+    <div>
+      <h1>TeacherList</h1>
+    </div>
+  );
+}
