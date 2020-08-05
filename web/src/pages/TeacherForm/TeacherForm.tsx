1bb59dc63df30f719d22af12ca40c92af92fdd12
Furtado <furttado@gmail.com>
2020-08-05 10:16:51 -0300

    WEB: page TeacherForm HTML & basic CSS; component Input created

--- web/src/pages/TeacherForm/TeacherForm.tsx
+++ web/src/pages/TeacherForm/TeacherForm.tsx
@@ -1,9 +1,36 @@
 import React from "react";
+import PageHeader from "../../components/PageHeader";
+
+import "./style.css";
 
 export default function TeacherForm() {
   return (
-    <div>
-      <h1>TeacherForm</h1>
+    <div id="page-teacher-form" className="container">
+      <PageHeader
+        title="How amazing you want to teach."
+        description="The first step is to fill out this application form."
+      />
+
+      <main>
+        <fieldset>
+          <legend>Your data</legend>
+
+          <div className="input-block">
+            <label htmlFor="name">Full name</label>
+            <input type="text" id="name" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="avatar">Avatar</label>
+            <input type="text" id="avatar" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="whatsapp">Whatsapp</label>
+            <input type="text" id="whatsapp" />
+          </div>
+        </fieldset>
+      </main>
     </div>
   );
 }
