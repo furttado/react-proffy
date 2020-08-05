d3f0d70de11134d76a064ba5be74db3b46302ce2
Furtado <furttado@gmail.com>
2020-08-05 20:15:32 -0300

    SERVER: port changed

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,7 @@
 import app from "./index";
 import { AddressInfo } from "net";
 
-const server = app.listen(process.env.PORT || 3000, () => {
+const server = app.listen(process.env.PORT || 3333, () => {
   if (server) {
     const address = server.address() as AddressInfo;
     console.log(`Server is runing in http://localhost:${address.port}`);
