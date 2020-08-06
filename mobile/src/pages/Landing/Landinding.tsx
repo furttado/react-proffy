559a146a552510e8d913e6353ac17cf181f2120a
Furtado <furttado@gmail.com>
2020-08-06 11:02:23 -0300

    buttons added to Landing

--- mobile/src/pages/Landing/Landinding.tsx
+++ mobile/src/pages/Landing/Landinding.tsx
@@ -1,17 +1,42 @@
 import React from "react";
-import { View, Image, Text } from "react-native";
+import { View, Image, Text, TouchableOpacity } from "react-native";
 
 import styles from "./styles";
 import landingImg from "../../assets/images/landing.png";
+import studyIcon from "../../assets/images/icons/study.png";
+import giveClassesIcon from "../../assets/images/icons/give-classes.png";
+import heartIcon from "../../assets/images/icons/heart.png";
 
 export default function Landing() {
   return (
     <View style={styles.container}>
       <Image source={landingImg} style={styles.banner} />
+
       <Text style={styles.title}>
         Wellcome, {"\n"}
         <Text style={styles.titleBold}>What would you like to do?</Text>
       </Text>
+
+      <View style={styles.buttonsContainer}>
+        <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+          <Image source={studyIcon} />
+          <Text style={styles.buttonText}>Learn</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, styles.buttonSecondary]}>
+          <Image source={giveClassesIcon} />
+          <Text style={styles.buttonText}>Teach</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
+/**
+ <RectButton
+          onPress={handleNavigateToGiveClssesPage}
+          style={[styles.button, styles.buttonSecondary]}
+        >
+          <Image source={giveClassesIcon} />
+          <Text style={styles.buttonText}>Dar aulas</Text>
+        </RectButton>
+ */
