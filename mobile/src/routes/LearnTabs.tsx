3c05e7b0646fc399f7c4d1418317d00d6e2c537c
Furtado <furttado@gmail.com>
2020-08-06 14:59:35 -0300

    LearnTabs, Favorites and TeacherList styling done

--- mobile/src/routes/LearnTabs.tsx
+++ mobile/src/routes/LearnTabs.tsx
@@ -9,7 +9,34 @@ const { Navigator, Screen } = createBottomTabNavigator();
 
 function LearnTabs() {
   return (
-    <Navigator>
+    <Navigator
+      tabBarOptions={{
+        style: {
+          elevation: 0,
+          shadowOpacity: 0,
+          height: 64,
+        },
+        tabStyle: {
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+        },
+        iconStyle: {
+          flex: 0,
+          width: 20,
+          height: 20,
+        },
+        labelStyle: {
+          fontFamily: "Archivo_700Bold",
+          fontSize: 13,
+          marginLeft: 16,
+        },
+        inactiveBackgroundColor: "#fafafc",
+        activeBackgroundColor: "#ebebf5",
+        inactiveTintColor: "#c1bccc",
+        activeTintColor: "#32264d",
+      }}
+    >
       <Screen name="TeacherList" component={TeacherList} />
       <Screen name="Favorites" component={Favorites} />
     </Navigator>
