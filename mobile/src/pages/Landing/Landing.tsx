b9bda8bcefdff4e0d8182cd79e29c5cb2e5f4b11
Furtado <furttado@gmail.com>
2020-08-07 10:50:32 -0300

    Landing: total connections api integration done

--- mobile/src/pages/Landing/Landing.tsx
+++ mobile/src/pages/Landing/Landing.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Image, Text, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { RectButton } from "react-native-gesture-handler";
@@ -8,9 +8,19 @@ import landingImg from "../../assets/images/landing.png";
 import studyIcon from "../../assets/images/icons/study.png";
 import giveClassesIcon from "../../assets/images/icons/give-classes.png";
 import heartIcon from "../../assets/images/icons/heart.png";
+import api from "../../services/api";
 
 export default function Landing() {
   const { navigate } = useNavigation();
+
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    async function getTotalConnections() {
+      const response = await api.get("connections");
+      const { total } = response.data;
+    }
+  }, []);
 
   function handleNavigateToTeachPage() {
     navigate("Teach");
@@ -48,7 +58,7 @@ export default function Landing() {
       </View>
 
       <Text style={styles.totalConnections}>
-        Total of 285 connections already made {" " /**/}
+        Total of {totalConnections} connections already made {" " /**/}
         <Image source={heartIcon} />
       </Text>
     </View>
